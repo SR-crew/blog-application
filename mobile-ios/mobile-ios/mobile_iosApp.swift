d4953641aae835f7f8b48f8f816ba095d69b0495
Alisher Baigazin <alisherbaigazin@gmail.com>
2023-02-14 00:15:42 +0600

    initial commit: basic setup for ios app with tests

---
+++ mobile-ios/mobile-ios/mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_iosApp.swift
+//  mobile-ios
+//
+//  Created by Alisher Baigazin on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
