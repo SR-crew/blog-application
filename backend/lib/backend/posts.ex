29e45022a053932a4335c6d60d56e2bd1cc26991
Sabit <sabit990928@gmail.com>
2023-03-03 02:00:22 +0600

    Add implementation and tests to get list of posts

--- backend/lib/backend/posts.ex
+++ backend/lib/backend/posts.ex
@@ -2,4 +2,15 @@ defmodule Backend.Posts do
   @moduledoc """
   The Posts context.
   """
+
+  alias Backend.Repo
+  alias Backend.Posts.Post
+
+  @doc """
+  Returns the list of posts.
+  """
+  @spec list_posts :: list()
+  def list_posts do
+    Repo.all(Post)
+  end
 end
--- backend/test/backend/posts_test.exs
+++ backend/test/backend/posts_test.exs
@@ -1,9 +1,24 @@
 defmodule Backend.PostsTest do
   use Backend.DataCase
 
+  import Backend.Factory
+
   alias Backend.Posts
 
   test "list_posts/0 returns all posts" do
-    assert [] == Posts.list_posts()
+    post =
+      insert!(:post,
+        title: "App signal implementation",
+        summary: "App signal implementation summary",
+        content: "App signal implementation content",
+        read_duration: 5
+      )
+
+    assert [post] == Posts.list_posts()
+
+    assert post.title == "App signal implementation"
+    assert post.summary == "App signal implementation summary"
+    assert post.content == "App signal implementation content"
+    assert post.read_duration == 5
   end
 end
